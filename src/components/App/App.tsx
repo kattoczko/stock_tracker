e32af6d29d00142bc42d6d8a667cd5da640b16be
Katarzyna <kasia.oczkowska@gmail.com>
2019-09-09 18:14:00 +0800

    Created basic navbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+
 import CompaniesPage from "../CompaniesPage/CompaniesPage";
 import TrackNewCompanyPage from "../TrackNewCompanyPage/TrackNewCompanyPage";
 import styles from "./App.module.css";
 
 const App: React.FunctionComponent<{}> = () => {
   return (
-    <div>
+    <div className={styles.app}>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={CompaniesPage} />
         <Route path="/track-new" component={TrackNewCompanyPage} />
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const NavBar: React.FunctionComponent<{}> = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+        <Link to="/">Stock Tracker</Link>
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <LinkContainer to="/">
+          <Nav.Link>Companies</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/track-new">
+          <Nav.Link>Track new company</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default NavBar;
