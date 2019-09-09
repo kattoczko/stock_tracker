979c2969380055a248c11d170d305f81e5570b44
Katarzyna <kasia.oczkowska@gmail.com>
2019-09-09 18:14:26 +0800

    Created form scaffold for adding new company to track

--- src/components/TrackNewCompanyPage/TrackNewCompanyPage.tsx
+++ src/components/TrackNewCompanyPage/TrackNewCompanyPage.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const TrackNewCompanyPage: React.FunctionComponent<{}> = () => {
-  return <div>Track new company</div>;
+  return (
+    <div>
+      <h1>Track new company</h1>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Company symbol</Form.Label>
+          <Form.Control type="text" placeholder="Company symbol" />
+          <Form.Text className="text-muted">
+            Provide the stock exchange symbol of a company you want to track
+          </Form.Text>
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={() => console.log("click")}
+        >
+          Track
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default TrackNewCompanyPage;
