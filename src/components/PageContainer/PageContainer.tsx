d36f73c640c1d69d2623f09c075f28d1232192e7
Katarzyna <kasia.oczkowska@gmail.com>
2019-09-10 15:37:25 +0800

    Moved styles to css module

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./PageContainer.module.css";
 
 const PageContainer: React.FunctionComponent<{}> = ({ children }) => {
-  return <div style={{ padding: "20px" }}>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default PageContainer;
