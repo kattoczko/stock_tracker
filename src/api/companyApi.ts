0870e6dda2e40a3aa9ba4a225789793f2d1d8c59
Katarzyna <kasia.oczkowska@gmail.com>
2019-09-10 16:09:41 +0800

    Moved api helper to separate file

--- src/api/companyApi.ts
+++ src/api/companyApi.ts
@@ -1,3 +1,5 @@
+import { handleResponse, handleError } from "./apiUtils";
+
 const baseUrl = process.env.REACT_APP_API_URL;
 const apiKey = process.env.REACT_APP_API_KEY;
 
@@ -15,18 +17,4 @@ export function getGlobalQuote(symbol: string) {
   )
     .then(handleResponse)
     .catch(handleError);
-}
-
-export async function handleResponse(response) {
-  if (response.ok) return response.json();
-  if (response.status === 400) {
-    const error = await response.text();
-    throw new Error(error);
-  }
-  throw new Error("Network response was not ok.");
-}
-
-export function handleError(error) {
-  console.error("API call failed. " + error);
-  throw error;
 }
