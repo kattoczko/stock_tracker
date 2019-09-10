91d2b73f0f6d0c428b200cdc6510bfd81cdfcc1a
Katarzyna <kasia.oczkowska@gmail.com>
2019-09-10 11:43:39 +0800

    Linked redux state to companies page

--- src/components/CompaniesPage/CompaniesPage.tsx
+++ src/components/CompaniesPage/CompaniesPage.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const CompaniesPage: React.FunctionComponent<{}> = () => {
-  return <div>Companies Page</div>;
+import { connect } from "react-redux";
+import { AppState } from "../../redux/store";
+import { CompaniesState } from "../../redux/companies/types";
+
+interface CompaniesPageProps {
+  companies: CompaniesState;
+}
+
+const CompaniesPage: React.FunctionComponent<CompaniesPageProps> = ({
+  companies
+}) => {
+  console.log(companies);
+  return (
+    <div>
+      <h1>Companies</h1>
+      {companies.length === 0 ? (
+        <div>
+          <p>
+            There are no companies yet.
+            <Link to="/track-new"> Track your first company.</Link>
+          </p>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
-export default CompaniesPage;
+function mapStateToProps(state: AppState) {
+  return {
+    companies: state.companies
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(CompaniesPage);
